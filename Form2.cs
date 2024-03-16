e01464914e3f5f043475bd5bea8877c3627b4485
David <davidmm_90@hotmail.com>
2024-03-15 19:45:12 -0600

    Pantalla de Bienvenida

---
+++ Form2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto___DAS
+{
+    public partial class Bienvenida : Form
+    {
+        public Bienvenida()
+        {
+            InitializeComponent();
+        }
+
+        private void copylbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Bienvenida_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar.Width += 3;
+
+            if (progressBar.Width >= 599)
+            {
+                timer1.Stop();
+                Form1 form1 = new Form1();
+                form1.Show();
+                this.Hide();
+            }
+        }
+    }
+    
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Proyecto___DAS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Bienvenida());
         }
     }
 }
