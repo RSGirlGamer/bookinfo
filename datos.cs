328073af590b8216928efc70fd4f1e27b92b4d05
jorgeee0-e <gonzalezmenjivar.5@gmail.com>
2024-03-15 20:33:26 -0600

    un metodo por categoria

--- datos.cs
+++ datos.cs
@@ -148,12 +148,70 @@ namespace Proyecto___DAS
 
         }
 
-      
 
+        static void definePersonas()
+        {
+            List<LoginInfo> listaPersonas = new List<LoginInfo>();
 
+            listaPersonas.Add(new LoginInfo
+            {
+                Nombre = "Juan",
+                Apellido = "Pérez",
+                FechaNacimiento = new DateTime(1990, 5, 15),
+                Preferencia = "Lenguajes de Programación",
+                Username = "juanperez",
+                Password = "contraseña123"
+            });
 
+            listaPersonas.Add(new LoginInfo
+            {
+                Nombre = "María",
+                Apellido = "Gómez",
+                FechaNacimiento = new DateTime(1985, 10, 20),
+                Preferencia = "Libros",
+                Username = "mariagomez",
+                Password = "librospass"
+            });
 
+            listaPersonas.Add(new LoginInfo
+            {
+                Nombre = "Carlos",
+                Apellido = "López",
+                FechaNacimiento = new DateTime(1995, 3, 8),
+                Preferencia = "Programas de Entretenimiento",
+                Username = "carloslopez",
+                Password = "entretenimientopass"
+            });
 
+            listaPersonas.Add(new LoginInfo
+            {
+                Nombre = "Ana",
+                Apellido = "Martínez",
+                FechaNacimiento = new DateTime(1988, 7, 12),
+                Preferencia = "Lenguajes de Programación",
+                Username = "anamartinez",
+                Password = "lenguajespass"
+            });
 
+            listaPersonas.Add(new LoginInfo
+            {
+                Nombre = "Pedro",
+                Apellido = "Rodríguez",
+                FechaNacimiento = new DateTime(1992, 9, 25),
+                Preferencia = "Libros",
+                Username = "pedrorodriguez",
+                Password = "pass1234"
+            });
+
+        }
     }
+
+
+
+
+
+
+
+
 }
+
